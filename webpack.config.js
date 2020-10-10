12b58892f4f3f3d245cc115576a8acca6db674aa
hanifrev <hanifrev@gmail.com>
2020-10-10 10:00:44 +0700

    add eslint, prettier, public

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
 
   output: {
-    path: path.resolve(__dirname, "/dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'bundle.js'
   },
   resolve: {
-    modules: [path.join(__dirname, "src"), "node_modules"],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
     alias: {
-      react: path.join(__dirname, "node_modules", "react"),
-    },
+      react: path.join(__dirname, 'node_modules', 'react')
+    }
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 8080,
+    port: 8080
   },
   module: {
     rules: [
@@ -25,27 +25,27 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader",
-          },
-        ],
-      },
-    ],
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      chunks: ["main"],
-    }),
-  ],
-};
+      template: './public/index.html',
+      filename: 'index.html',
+      chunks: ['main']
+    })
+  ]
+}
